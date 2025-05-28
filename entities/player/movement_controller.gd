e7eb9040f89a9d7a4e88dadc562a2134c6f751e1
Neal Wang <nealwang.sh@protonmail.com>
2025-05-27 17:01:57 -0700

    feat: looking around

--- entities/player/movement_controller.gd
+++ entities/player/movement_controller.gd
@@ -1,11 +1,14 @@
 extends Node
 
 @export var player: CharacterBody3D
+@export var yaw: Node3D
 @export var mesh_root: Node3D
 @export var rotation_speed: float = 8
 @export var fall_gravity: float = 45
+@export var rotation_deadzone: float = 2
 
 var direction: Vector3
+var last_rotation: float
 var velocity: Vector3
 var acceleration: float
 var speed: float
@@ -13,8 +16,15 @@ var jump_gravity := fall_gravity
 
 
 func _physics_process(delta):
-	velocity.x = speed * direction.normalized().x
-	velocity.z = speed * direction.normalized().z
+	# allow player to "look" around by remembering previous yaw
+	if Input.is_action_just_pressed("look"):
+		last_rotation = yaw.rotation.y
+
+	var rotation := last_rotation if Input.is_action_pressed("look") else yaw.rotation.y
+	var global_direction := direction.rotated(Vector3.UP, rotation)
+
+	velocity.x = speed * global_direction.normalized().x
+	velocity.z = speed * global_direction.normalized().z
 
 	if not player.is_on_floor():
 		if velocity.y >= 0:
@@ -25,8 +35,11 @@ func _physics_process(delta):
 	player.velocity = player.velocity.lerp(velocity, acceleration * delta)
 	player.move_and_slide()
 
-	var target_rotation = atan2(direction.x, direction.z) - player.rotation.y
-	mesh_root.rotation.y = lerp_angle(mesh_root.rotation.y, target_rotation, rotation_speed * delta)
+	if velocity.length() > rotation_deadzone:
+		var target_rotation = atan2(global_direction.x, global_direction.z) - player.rotation.y
+		mesh_root.rotation.y = lerp_angle(
+			mesh_root.rotation.y, target_rotation, rotation_speed * delta
+		)
 
 
 func _on_set_movement_state(movement_state: MovementState):
