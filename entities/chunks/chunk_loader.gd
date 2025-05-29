d6f377856f44394a35c480d583f80d1d3953be77
Neal Wang <nealwang.sh@protonmail.com>
2025-05-29 01:05:50 -0700

    feat: water shader; textures

--- entities/chunks/chunk_loader.gd
+++ entities/chunks/chunk_loader.gd
@@ -6,6 +6,6 @@ var chunk_scene := preload("res://entities/chunks/chunk.tscn")
 func _ready():
 	for i in range(0, 10):
 		var chunk: Chunk = chunk_scene.instantiate()
-		chunk.position.x = i * 40
+		chunk.position.z = -i * 40
 		chunk.address = str(i)
 		add_child(chunk)
