b4035f7cf9d8037a701cb5d5453c9c1137e9afed
Neal Wang <nealwang.sh@protonmail.com>
2025-05-29 20:54:37 -0700

    feat: load_street()

--- entities/chunks/chunk.gd
+++ entities/chunks/chunk.gd
@@ -1,8 +1,12 @@
 extends Node3D
 class_name Chunk
 
-const CHUNK_WIDTH: int = 8
-const CHUNK_DEPTH: int = 8
+const CHUNK_WIDTH: int = 40
+const CHUNK_DEPTH: int = 40
+const CHUNK_ROWS: int = 8
+const CHUNK_COLS: int = 8
+const MARGIN_X: float = (CHUNK_WIDTH - Pillar.PILLAR_WIDTH * CHUNK_COLS) / 2.0
+const MARGIN_Y: float = (CHUNK_DEPTH - Pillar.PILLAR_DEPTH * CHUNK_ROWS) / 2.0
 
 var pillar_scene := preload("res://entities/chunks/pillar.tscn")
 
@@ -15,11 +19,11 @@ func _ready():
 	ctx.update(address.to_utf8_buffer())
 	var digest := ctx.finish()
 
-	for i in range(0, CHUNK_WIDTH):
-		for j in range(0, CHUNK_DEPTH):
+	for i in range(0, CHUNK_ROWS):
+		for j in range(0, CHUNK_COLS):
 			# index into the hash by doubles
 			@warning_ignore("integer_division")
-			var nibble := digest[(i * CHUNK_DEPTH + j) / 2]
+			var nibble := digest[(i * CHUNK_COLS + j) / 2]
 			if j % 2 == 0:
 				# take first half of the byte
 				nibble = nibble >> 4
@@ -29,8 +33,9 @@ func _ready():
 
 			var pillar: Pillar = pillar_scene.instantiate()
 			pillar.name = str(i) + "_" + str(j)
-			pillar.position.x = 4 + i * Pillar.PILLAR_WIDTH
-			pillar.position.z = 4 + j * Pillar.PILLAR_DEPTH
+			# INFO: chunk is rotated 90 degrees
+			pillar.position.z = MARGIN_X + j * Pillar.PILLAR_WIDTH
+			pillar.position.x = MARGIN_Y + i * Pillar.PILLAR_DEPTH
 			pillar.datum = nibble
-			pillar.delay = (abs(CHUNK_WIDTH / 2.0 - i) + abs(CHUNK_DEPTH / 2.0 - j)) * 200
+			pillar.delay = (abs(CHUNK_ROWS / 2.0 - i) + abs(CHUNK_COLS / 2.0 - j)) * 200
 			add_child(pillar)
--- entities/chunks/chunk_loader.gd
+++ entities/chunks/chunk_loader.gd
@@ -1,11 +1,74 @@
 extends Node
 
+const CHARSET := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+var charset := {}
+
+@export var player: Node3D
+@export var render_distance: int = 4
+
 var chunk_scene := preload("res://entities/chunks/chunk.tscn")
+
+var last_chunk: Vector2
 
 
 func _ready():
-	for i in range(0, 10):
-		var chunk: Chunk = chunk_scene.instantiate()
-		chunk.position.z = -i * 40
-		chunk.address = str(i)
-		add_child(chunk)
+	for i in CHARSET.length():
+		charset[CHARSET[i]] = i
+
+	last_chunk = world_to_chunk(player.global_transform.origin)
+
+	load_street(Vector2(0, 0), "A")
+
+
+func _physics_process(delta: float):
+	var chunk := world_to_chunk(player.global_transform.origin)
+
+
+func world_to_chunk(world_pos: Vector3) -> Vector2:
+	return Vector2(floor(world_pos.x / Chunk.CHUNK_WIDTH), -floor(world_pos.z / Chunk.CHUNK_DEPTH))
+
+
+func chunk_to_world(chunk: Vector2) -> Vector3:
+	return Vector3(chunk.x * Chunk.CHUNK_WIDTH, 0, -chunk.y * Chunk.CHUNK_DEPTH)
+
+
+func load_street(chunk: Vector2, address: String):
+	var code := encode(address)
+	# go up and down
+	for i in range(-render_distance, render_distance + 1):
+		var new_chunk := Vector2(chunk.x, chunk.y + i)
+		var new_address := decode(code + i)
+		if new_address.length() != address.length():
+			# underflow or overflow
+			continue
+
+		async_spawn_chunk(new_chunk, new_address, abs(i) * 0.5)
+
+
+func async_spawn_chunk(chunk: Vector2, address: String, delay: float = 0.0):
+	await get_tree().create_timer(delay).timeout
+	spawn_chunk(chunk, address)
+
+
+func spawn_chunk(chunk: Vector2, address: String):
+	var new_chunk: Chunk = chunk_scene.instantiate()
+	new_chunk.address = address
+	new_chunk.transform.origin = chunk_to_world(chunk)
+	add_child(new_chunk)
+
+
+func encode(address: String) -> int:
+	var res := 0
+	for c in address:
+		# shift by 1 to avoid breaking bijective mapping
+		res = res * CHARSET.length() + charset[c] + 1
+	return res
+
+
+func decode(code: int) -> String:
+	var res := ""
+	while code > 0:
+		code -= 1
+		res += CHARSET[code % CHARSET.length()]
+		code /= CHARSET.length()
+	return res
